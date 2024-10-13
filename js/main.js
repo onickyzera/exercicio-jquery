d01f609da96d9af6ec1d7e0fc6c4b8cd804bceff
Nícolas <linky152@gmail.com>
2024-10-13 14:38:04 -0300

    commit initial

---
+++ js/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+    $("#carousel-imagens").slick({
+        autoplay:true,
+        arrows:false
+    });
+
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(DDD) 12345-6789'
+    });
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    });
+
+    $('#cep').mask('00000-000', {
+        placeholder: '012345-678'
+    });
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            }
+        },
+        messages: {
+            nome: "Por favor, insira o seu nome",
+            telefone: "Por favor, insira seu telefone",
+            email: "Por favor, insira seu E-mail",
+            endereco: "Por favor, insira seu Endereço",
+            cep: "Por favor, insira seu CEP",
+            cpf: "Por favor, insira seu CPF"
+        },
+        submitHandler: function (form) {
+            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+            form.reset();
+        },
+        invalidHandler: function(form, validator) {
+            let camposIncorretos = validator.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Por favor, preencha os ${camposIncorretos} campos para prosseguir com a compra!`);
+            }
+        }
+    });
+});
